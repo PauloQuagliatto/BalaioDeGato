d83b1572a114bf0a9565d5fa79de004ec24717c7
Matheus Moura <matheus.moura13@gmail.com>
2019-01-26 14:09:38 -0300

    Refatora maquina de estados

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -10,7 +10,8 @@ public class Player : MonoBehaviour
 		IDDLE,
 		MOVE,
 		INTERACT,
-		DIE
+		DIE,
+		JUMP
 	}
 
 	[Header("Velocidade")]
@@ -20,6 +21,7 @@ public class Player : MonoBehaviour
 
 	[Header("Trigging")]
 	public PlayerState currentState = PlayerState.IDDLE;
+	private PlayerState lastState = PlayerState.IDDLE;
 	public LayerMask layer;
 	public float rayDistance = 1.18f;
 
@@ -38,44 +40,132 @@ public class Player : MonoBehaviour
 		myRigidbody2D = GetComponent<Rigidbody2D>();
 	}
 
+	void Update()
+	{
+		float horizontalAxis = Input.GetAxis("Horizontal");
+		Collider2D[] interactColliders = getCollidersAroundInteractPoint();
+
+		switch (currentState)
+		{
+			case PlayerState.IDDLE:
+				{
+					// Chamada das funções principais
+					Iddle();
+					// Gatilhos para mudar de estado
+					if (horizontalAxis != 0) ChangeState(PlayerState.MOVE);
+					if (Input.GetButton("Jump") && HasColliderBottom()) ChangeState(PlayerState.JUMP);
+					if (Input.GetButton("Fire1") && interactColliders.Length != 0) ChangeState(PlayerState.INTERACT);
+					break;
+				}
+			case PlayerState.MOVE:
+				{
+					// Chamada das funções principais
+					Move(horizontalAxis);
+					// Gatilhos para mudar de estado
+					if (horizontalAxis == 0) ChangeState(PlayerState.IDDLE);
+					if (Input.GetButton("Jump") && HasColliderBottom()) ChangeState(PlayerState.JUMP);
+					if (Input.GetButton("Fire1") && interactColliders.Length != 0) ChangeState(PlayerState.INTERACT);
+					break;
+				}
+			case PlayerState.JUMP:
+				{
+					// Chamada das funções principais
+					Jump();
+					// Gatilhos para mudar de estado
+					ChangeState(lastState);
+					break;
+				}
+			case PlayerState.INTERACT:
+				{
+					// Chamada das funções princiais
+					Interact(interactColliders);
+					// Gatilhos para mudar de estado
+					ChangeState(lastState);
+					break;
+				}
+		}
+		FlipDirection(horizontalAxis);
+	}
+
+	#region Maquina de estado
+
 	/// <summary>
 	/// Altera o estado da maquina de estados
 	/// </summary>
 	/// <param name="newState">Novo estado que será aplicado</param>
 	void ChangeState(PlayerState newState)
 	{
+		lastState = currentState;
 		currentState = newState;
+	}
+
+	/// <summary>
+	/// Aplica um efeito nos elementos
+	/// </summary>
+	private void Interact(Collider2D[] colliders)
+	{
+		foreach (Collider2D collider in colliders)
+		{
+			InteragableObject interagableObject = collider.GetComponent<InteragableObject>();
+			// Aplicamos uma força
+			interagableObject.ApplyForce(transform.position + interactionPosition, direction);
+		}
+	}
+
+	/// <summary>
+	/// Estado de iddle da maquina de estados, aqui o jogador está totalmente parado
+	/// aguardando por inputs para se mover
+	/// </summary>
+	private void Iddle()
+	{
+		// TODO - aplicar animação e funções de idle aqui
 	}
 
 	/// <summary>
 	/// Movimenta o player na cena
 	/// </summary>
-	private void Move()
+	/// <param name="horizontalMove">Movimento horizontal que será aplicado</param>
+	private void Move(float horizontalMove)
 	{
-		float horizontalAxis = Input.GetAxis("Horizontal");
-		transform.Translate(horizontalAxis * moveSpeed * Time.deltaTime, 0, 0);
-		if (Input.GetButtonDown("Jump") && HasColliderBottom()) Jump();
-		if (Input.GetButtonDown("Fire1")) Hit();
-
-		FlipDirection(horizontalAxis);
+		transform.Translate(horizontalMove * moveSpeed * Time.deltaTime, 0, 0);
 	}
 
 	/// <summary>
-	/// Aplica um efeito nos elementos que estiverem no raio de colisão
+	/// Aplica uma força no eixo y do rigidbody, fazendo que o o player salte
 	/// </summary>
-	private void Hit()
+	private void Jump()
 	{
-		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position+interactionPosition, interactionRange, layerMaskObjects);
-		foreach(Collider2D collider in colliders)
-		{
-			InteragableObject interagableObject = collider.GetComponent<InteragableObject>();
-			interagableObject.ApplyForce(transform.position + interactionPosition, direction);
-		}
+		myRigidbody2D.velocity = new Vector2(myRigidbody2D.velocity.x, 0);
+		myRigidbody2D.AddForce(new Vector2(0, jumpForce * Time.deltaTime), ForceMode2D.Impulse);
 	}
 
+	#endregion
+
+	#region Funções auxiliares
 
 	/// <summary>
-	/// Altera a escala x do objeto atual para dar um flip em todos os elementos
+	/// Pega os colisores de objetos de interação ao redor do player
+	/// </summary>
+	private Collider2D[] getCollidersAroundInteractPoint()
+	{
+		return Physics2D.OverlapCircleAll(transform.position + interactionPosition, interactionRange, layerMaskObjects);
+	}
+
+	/// <summary>
+	/// Verifica se possui algum collider embaixo, respeitando a layermask definida
+	/// </summary>
+	/// <returns>Se o player está no chão ou não</returns>
+	private bool HasColliderBottom()
+	{
+		Vector3 targetPosition = transform.position - (transform.up * rayDistance);
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, targetPosition, rayDistance, layer);
+
+		return hit.collider != null;
+	}
+
+	/// <summary>
+	/// Checa a direção atual da sprite da axis e altera a escala x
+	/// do objeto atual para dar um flip em todos os elementos
 	/// </summary>
 	/// <param name="horizontalValue">Valor da axi horizontal</param>
 	private void FlipDirection(float horizontalValue)
@@ -93,34 +183,9 @@ public class Player : MonoBehaviour
 		transform.localScale = new Vector3(direction, 1, 1);
 	}
 
-	/// <summary>
-	/// Aplica uma força no eixo y do rigidbody, fazendo que o o player salte
-	/// </summary>
-	private void Jump()
-	{
-		myRigidbody2D.velocity = new Vector2(myRigidbody2D.velocity.x, 0);
-		myRigidbody2D.AddForce(new Vector2(0, jumpForce * Time.deltaTime), ForceMode2D.Impulse);
-	}
+	#endregion
 
-	/// <summary>
-	/// Verifica se possui algum collider embaixo, respeitando a layermask definida
-	/// </summary>
-	/// <returns>Se o player está no chão ou não</returns>
-	private bool HasColliderBottom()
-	{
-		Vector3 targetPosition = transform.position - (transform.up * rayDistance);
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, targetPosition, rayDistance, layer);
-
-		return hit.collider != null;
-	}
-
-	void Update()
-	{
-		switch (currentState)
-		{
-			case PlayerState.MOVE: Move(); break;
-		}
-	}
+	#region Funções de debug
 
 	/// <summary>
 	/// Utilizamos o Gizmoz do unity para debugar os raios de colisao
@@ -136,4 +201,6 @@ public class Player : MonoBehaviour
 		Gizmos.color = Color.blue;
 		Gizmos.DrawWireSphere(transform.position + interactionPosition, interactionRange);
 	}
+
+	#endregion
 }
