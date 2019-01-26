ef2f12bba9f2737ef765c311efe482930302915b
Matheus Moura <matheus.moura13@gmail.com>
2019-01-26 03:36:23 -0300

    Ajusta prefab do player com nova distancia de raycast

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-
 	public enum PlayerState
 	{
 		IDDLE,
@@ -14,10 +14,24 @@ public class Player : MonoBehaviour
 		DIE
 	}
 
-	[Header("Configurações do player")]
+	[Header("Configurações de velocidade")]
 	public float moveSpeed = 10;
+	public float jumpForce = 100;
+
+	[Header("Configurações de trigging")]
+	public float groundRayDistance = 3;
+
+	private Rigidbody2D rigidbody2D;
+
+	// flags
+	private bool inGround = false;
 
 	public PlayerState currentState = PlayerState.IDDLE;
+
+	private void Start()
+	{
+		rigidbody2D = GetComponent<Rigidbody2D>();
+	}
 
 	/// <summary>
 	/// Altera o estado da maquina de estados
@@ -34,6 +48,21 @@ public class Player : MonoBehaviour
 	private void Move()
 	{
 		transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, 0);
+		if (Input.GetButtonDown("Jump")) Jump();
+	}
+
+	/// <summary>
+	/// Faz o player dar um pulo
+	/// </summary>
+	private void Jump()
+	{
+		rigidbody2D.velocity = new Vector2(0, 0);
+		rigidbody2D.AddForce(new Vector2(0, jumpForce * Time.deltaTime), ForceMode2D.Impulse);
+	}
+
+	private void CheckIsInGround()
+	{
+		// RayCastHit2D hit = Physics2D.Raycast(transform.position, transform.position * (Vector2.up * groundRayDistance), groundRayDistance);
 	}
 
 	void FixedUpdate()
@@ -42,6 +71,12 @@ public class Player : MonoBehaviour
 		{
 			case PlayerState.MOVE: Move(); break;
 		}
+	}
+
+	private void OnDrawGizmos()
+	{
+		// Desenha a linha de debug do raycast
+		Gizmos.DrawLine(transform.position, transform.position * (Vector2.up * groundRayDistance));
 	}
 
 
