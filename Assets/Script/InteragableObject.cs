ba4cd19b9b94421429aba6758dff2ac228e9fb38
Matheus Moura <matheus.moura13@gmail.com>
2019-01-26 08:58:47 -0300

    Implementa script de interação com objetos do cenário

---
+++ Assets/Script/InteragableObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class InteragableObject : MonoBehaviour
+{
+
+	private Rigidbody2D myRigidbody2D;
+
+	void Start()
+	{
+		myRigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	/// <summary>
+	/// Desativa a fisica do objeto
+	/// </summary>
+	/// <param name="value"></param>
+	public void TogglePhysics(bool value)
+	{
+		if (value) myRigidbody2D.velocity = Vector2.zero;
+		myRigidbody2D.isKinematic = value;
+	}
+}
