7cbb6ebc7542cc0f8bf04ddc7ec8c9af23a00c8c
Matheus Moura <matheus.moura13@gmail.com>
2019-01-27 08:42:47 -0300

    Refatora esfera de interação da IA e implementa movimentação/idle

--- Assets/Script/IAController.cs
+++ Assets/Script/IAController.cs
@@ -11,15 +11,121 @@ public class IAController : MonoBehaviour
 		MOVING,
 		INTERACT,
 		BITED,
-		DIE,
-		HITED
+		DIE
 	}
 
-	[Header("Informações dos sensores")]
-	public float maxMoveDistance = 2;
-	public float maxInteractDistance = 5;
-		
 	public IAState currentState = IAState.IDLE;
+	[Header("Movimentação")]
+	public float moveSpeed = 5;
+	public float maxMoveDistance = 2;
+
+	[Header("Temporização")]
+	public float minRandomTime = 0;
+	public float maxRandomTime = 7;
+
+	[Header("Interação")]
+	public LayerMask layerMaskObjects;
+	public float maxInteractDistance = 5;
+	public Vector3 interactionPosition;
+	public float interactionRange = 1;
+
+	private float targetTime = 0;
+	private Vector2 targetPosition = Vector2.zero;
+
+	/// <summary>
+	/// Altera o estado da maquina de estados
+	/// </summary>
+	/// <param name="newState">Novo estado que será definido</param>
+	private void ChangeState(IAState newState)
+	{
+		currentState = newState;
+	}
+
+	private void Start()
+	{
+		UpdateTargetTime();
+	}
+
+	/// <summary>
+	/// Atualiza o tempo randomico
+	/// </summary>
+	private void UpdateTargetTime()
+	{
+		targetTime = Time.time + Random.Range(minRandomTime, maxRandomTime);
+	}
+
+	private void Update()
+	{
+		switch (currentState)
+		{
+			case IAState.IDLE:
+				{
+					// Gatilhos para mudar de estado
+					if (targetTime <= Time.time) {
+						targetPosition = transform.position;
+						targetPosition.x = Random.Range(transform.position.x-maxMoveDistance, transform.position.x + maxMoveDistance);
+						FlipDirection();
+						UpdateTargetTime();
+						ChangeState(IAState.MOVING);
+					}
+					break;
+				}
+			case IAState.MOVING:
+				{
+					// Chamada das funções principais
+					Move();
+					// Gatilhos para mudar de estado
+					if (targetTime <= Time.time)
+					{
+						UpdateTargetTime();
+						ChangeState(IAState.IDLE);
+					}
+					break;
+				}
+		}
+	}
+
+	private void OnTriggerEnter2D (Collider2D collider)
+	{
+		if (currentState == IAState.MOVING && collider.tag != "Ground") {
+			UpdateTargetTime();
+			ChangeState(IAState.IDLE);
+		}
+	}
+
+	/// <summary>
+	/// Move o player até uma posição especifica
+	/// </summary>
+	private void Move()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed*Time.deltaTime);
+	}
+
+	/// <summary>
+	/// Checa a direção atual da sprite da axis e altera a escala x
+	/// do objeto atual para dar um flip em todos os elementos
+	/// </summary>
+	private void FlipDirection()
+	{
+		int direction = 1;
+		if (targetPosition.x < transform.position.x)
+			direction = -1;
+
+		transform.localScale = new Vector3(direction, 1, 1);
+
+		interactionPosition.x = Mathf.Abs(interactionPosition.x);
+
+		if (interactionPosition.x > 0 && direction < 0)
+			interactionPosition.x *= direction;
+	}
+
+	/// <summary>
+	/// Pega os colisores de objetos de interação ao redor do player
+	/// </summary>
+	private Collider2D[] getCollidersAroundInteractPoint()
+	{
+		return Physics2D.OverlapCircleAll(transform.position + interactionPosition, interactionRange, layerMaskObjects);
+	}
 
 	private void OnDrawGizmos()
 	{
@@ -28,8 +134,9 @@ public class IAController : MonoBehaviour
 		Gizmos.color = Color.yellow;
 		Gizmos.DrawLine(currentPosition, targetPosition);
 
+		// Desenha o debug da area de interação
 		Gizmos.color = Color.blue;
-		Gizmos.DrawWireSphere(transform.position, maxInteractDistance);
+		Gizmos.DrawWireSphere(transform.position + interactionPosition, interactionRange);
 	}
 
 
