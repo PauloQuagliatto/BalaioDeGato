6ec8320ac06ad76ecf90d69739598ece51181ee8
Lucas <47045278+mecharazick@users.noreply.github.com>
2019-01-27 12:07:29 -0300

    Re-mapping colliders

--- Assets/Script/BoulderTrigger.cs
+++ Assets/Script/BoulderTrigger.cs
@@ -6,7 +6,7 @@ public class BoulderTrigger : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.collider.CompareTag("Player")|| collision.collider.CompareTag(""))
+        if(collision.collider.CompareTag("Player")|| collision.collider.CompareTag("Interagable"))
         Destroy(gameObject);
     }
 }
---
+++ Assets/Script/RamdomBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RamdomBox : MonoBehaviour
+{
+   [SerializeField] public GameObject pepino;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player") || collision.collider.CompareTag("Interagable"))
+            Destroy(gameObject);
+            pepino.SetActive(true);
+
+    }
+}
