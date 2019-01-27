2241ebc0cdaadc05708304c856d34a132ae2c6ef
Matheus Moura <matheus.moura13@gmail.com>
2019-01-27 12:10:33 -0300

    Remove colisores desnecessarios

--- Assets/Script/IAController.cs
+++ Assets/Script/IAController.cs
@@ -89,7 +89,8 @@ public class IAController : MonoBehaviour
 						// Gatilhos para mudar de estado
 						if (targetTime <= Time.time)
 						{
-							if (interagableColliders.Length != 0 && Random.Range(0, 3) == 1)
+							int randomNumber = Random.Range(0, 2);
+							if (interagableColliders.Length != 0 && (randomNumber == 0 || randomNumber == 1))
 							{
 								ChangeState(IAState.INTERACT);
 							}
@@ -137,17 +138,22 @@ public class IAController : MonoBehaviour
 		}
 	}
 
-	private void OnTriggerEnter2D (Collider2D collider)
+	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		if (collider.tag == "Damagable")
+		if (collision.gameObject.tag == "Damagable")
 		{
 			GameController.instance.Killed();
 			ChangeState(IAState.DIE);
-		} else if (currentState == IAState.MOVING && collider.tag != "Ground") {
+		}
+		else if (currentState == IAState.MOVING && collision.gameObject.tag != "Ground")
+		{
 			UpdateTargetTime();
 			ChangeState(IAState.IDLE);
 		}
+	}
 
+	private void OnTriggerEnter2D (Collider2D collider)
+	{
 		if(collider.tag == "SecureArea")
 		{
 			gameController.AddCatToSecurePoint();
