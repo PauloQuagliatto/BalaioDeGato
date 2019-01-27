98b97eb15936a576e37423d4e7a19145e34f004a
Matheus Moura <matheus.moura13@gmail.com>
2019-01-26 22:15:40 -0300

    Adiciona dos filhotes

---
+++ Assets/Script/IAController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAController : MonoBehaviour
+{
+
+	public enum IAState
+	{
+		IDLE,
+		MOVING,
+		INTERACT,
+		BITED,
+		DIE,
+		HITED
+	}
+
+	[Header("Informações dos sensores")]
+	public float maxMoveDistance = 2;
+	public float maxInteractDistance = 5;
+		
+	public IAState currentState = IAState.IDLE;
+
+	private void OnDrawGizmos()
+	{
+		Vector3 currentPosition = transform.position + (-Vector3.right * maxMoveDistance);
+		Vector3 targetPosition = transform.position + (Vector3.right * maxMoveDistance);
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawLine(currentPosition, targetPosition);
+
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere(transform.position, maxInteractDistance);
+	}
+
+
+}
